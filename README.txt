61bad4ff7b449fc98d3a4442cb389b04994a3e19
Takeo Hoshi <hoshi@tottori-u.ac.jp>
2022-06-22 00:31:52 +0900

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,10 +1,12 @@
 calc_eigen.py
 
-A small Python3 tool for calulating eigenvalues for the matrix files in the matrix-market format.
+A small Python3 tool for calulating hermitian or real-symmetric eigenvalues for the matrix files in the matrix-market format.
 
 Type 1: Generalized eigenvalue equation ( A v = e B v )
 Ex. python3 calc_eigen.py ELSES_MATRIX_BNZ30_A.mtx -b ELSES_MATRIX_BNZ30_B.mtx
 
 Type 2: Standard eigenvalue problem ( A v = e v )
 Ex. python3 calc_eigen.py ELSES_MATRIX_VCNT400std_A.mtx
+
+Note: This tool uses the dense-matrix solver library, scipy.linalg.eigh
 
--- calc_eigen.py
+++ calc_eigen.py
@@ -1,27 +1,25 @@
 import scipy
-import numpy
 import argparse
 from scipy import linalg
 from scipy.io import mmwrite, mmread
-import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('arg1', help='help for arg1', default='no_arg1')
 parser.add_argument('-b', help='help for arg2', default='no_B_matrix')
 
 args = parser.parse_args()
-print('matrix A = ',args.arg1)
-print('matrix B = ',args.b)
+#print('matrix A = ',args.arg1)
+#print('matrix B = ',args.b)
 
 if args.b != 'no_B_matrix' :
-    print('generalized eigenvalue problem')
+    print('Generalized eigenvalue problem')
     print('  matrix A : ',args.arg1)
     print('  matrix B : ',args.b)
     A=scipy.io.mmread(args.arg1).todense()
     B=scipy.io.mmread(args.b).todense()
     w, v = scipy.linalg.eigh(A,B)
 elif args.arg1 != 'no_arg1' :
-    print('standard eigenvalue problem')
+    print('Standard eigenvalue problem')
     print('  matrix A : ',args.arg1)
     A=scipy.io.mmread(args.arg1).todense()
     w, v = scipy.linalg.eigh(A)
