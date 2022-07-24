086595aef524f9ec3b156628a9e4b1e472510c43
Takeo Hoshi <hoshi@tottori-u.ac.jp>
2022-07-24 20:13:17 +0900

    Add files via upload

--- calc_eigen.py
+++ calc_eigen.py
@@ -1,13 +1,13 @@
 import scipy
 import argparse
-from scipy import linalg
+from scipy import linalg as LA
 from scipy.io import mmwrite, mmread
 
 parser = argparse.ArgumentParser()
-parser.add_argument('arg1', help='help for arg1', default='no_arg1')
-parser.add_argument('-b', help='help for arg2', default='no_B_matrix')
-parser.add_argument('-e', help='help for arg2', default='result_ev.txt')
-parser.add_argument('-ipr', help='help for arg2', default='result_ipr.txt')
+parser.add_argument('arg1', help='input file for A matrix', default='no_arg1')
+parser.add_argument('-b',   help='input file for B matrix', default='no_B_matrix')
+parser.add_argument('-e',   help='output file for eigenvalues', default='result_ev.txt')
+parser.add_argument('-ipr', help='output file for IPR', default='result_ipr.txt')
 
 args = parser.parse_args()
 #print('matrix A = ',args.arg1)
@@ -19,12 +19,12 @@ if args.b != 'no_B_matrix' :
     print('  matrix B : ',args.b)
     A=scipy.io.mmread(args.arg1).todense()
     B=scipy.io.mmread(args.b).todense()
-    w, v = scipy.linalg.eigh(A,B)
+    w, v = LA.eigh(A,B)
 elif args.arg1 != 'no_arg1' :
     print('Standard eigenvalue problem')
     print('  matrix A : ',args.arg1)
     A=scipy.io.mmread(args.arg1).todense()
-    w, v = scipy.linalg.eigh(A)
+    w, v = LA.eigh(A)
 else:
     print('ERROR:No input for matrix')
 
@@ -37,7 +37,7 @@ with open(args.ipr, 'w') as f:
         y=w
         for j in range(len(w)):
 #           print('i,j=',i,j)
-            w[j] = v[j,i]**4
+            w[j] = LA.norm(v[j,i])**4
         print (i+1, sum(w), file=f)
 
 
